c08ed8a21ce9b531b3eaf7dacea569d4f8ccf622
phayo <anazodogodwin@gmail.com>
2020-02-10 08:43:18 +0100

    camel setup

---
+++ src/main/java/com/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class TestClass {
+	private String name = "test";
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
